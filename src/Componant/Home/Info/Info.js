30df4446188d37ae96ef2651f26b09e263b37858
MDBADSHA7 <mahabubhossainfozlu@gmail.com>
2022-09-21 18:05:33 +0600

    card resolve

--- src/Componant/Home/Info/Info.js
+++ src/Componant/Home/Info/Info.js
@@ -7,10 +7,10 @@ const Info = () => {
     return (
         <div>
             <div className='mb-10'>
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-                    <InfoCard cardTittle="Opening Hours" bgclass="bg-gradient-to-r from-cyan-500 to-blue-500" img={clock}></InfoCard>
-                    <InfoCard cardTittle="Our Locations" bgclass="bg-gradient-to-r from-cyan-500 to-blue-500" img={marker}></InfoCard>
-                    <InfoCard cardTittle="Contact Us" bgclass="bg-gradient-to-r from-cyan-500 to-blue-500" img={phone}></InfoCard>
+                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                    <InfoCard cardTittle="Opening Hours" cardInfo="You can take appointment for anytime.There is no holiday of our serveces.You will get our services 24 hours except big issu !!" bgclass="bg-gradient-to-r from-cyan-500 to-blue-500" img={clock}></InfoCard>
+                    <InfoCard cardTittle="Our Locations" cardInfo="This is our Google location.If anyone want to get our services he can easily search our location by using this marker !!" bgclass="bg-gradient-to-r from-cyan-500 to-blue-500" img={marker}></InfoCard>
+                    <InfoCard cardTittle="Contact Us" cardInfo="This is our Phone information.If anyone want to booking appointment,he/she can contact with us by using phone call !!" bgclass="bg-gradient-to-r from-cyan-500 to-blue-500" img={phone}></InfoCard>
                 </div>
             </div>
             <hr />
--- src/Componant/Home/Info/InfoCard.js
+++ src/Componant/Home/Info/InfoCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
-const InfoCard = ({ img, cardTittle, bgclass }) => {
+const InfoCard = ({ img, cardTittle, bgclass, cardInfo }) => {
     return (
         <div class={`card lg:card-side bg-base-100 shadow-xl ${bgclass}`}>
-            <figure className='pl-5 pt-5'>
+            <figure className='pl-5 pb-5 pt-5'>
                 <img src={img} alt="Album" />
             </figure>
             <div class="card-body text-white">
-                <h2 class="card-title">{cardTittle}</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+                <h2 className="card-title">{cardTittle}</h2>
+                <h3 className='cardInfo'>{cardInfo}</h3>
             </div>
         </div>
     );
